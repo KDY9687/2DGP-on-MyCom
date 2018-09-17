45ce47a6dc4ec3bd6a5406e6dd1b1055874232c5
KDY_MyCom <jungsang3488@gmailc.om>
2018-09-17 22:26:53 +0900

    Drill05
    우측이동 함수 동작여부를 확인하기위해
    메인 함수에 삽입하였으나 마지막 위치에서 처음 위치로 돌아가지 않고 리스트 인덱스 오버플로우 버그발생
    
    **divided_x = (Point_List[index+1][0] - Point_List[index][0]) / 30 의 index+1이 마지막 위치에서 10이 되는걸 확인 Point_List 마지막에 맨 처음 좌표값 삽입으로 문제해결

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -3,8 +3,8 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-Point_List = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336],
-              [712, 349]]
+Point_List = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518],
+                [682, 336], [712, 349], [203, 535]]
 
 def move_right(divided_x, divided_y, index):
     frame = 0
@@ -32,8 +32,18 @@ def move_reft(divdivided_x, divided_y, index):
     pass
 
 def move_point_to_point(index):
-    divided_x = (Point_List[index+1][0] - Point_List[index][0]) / 30
+    divided_x = (Point_List[index + 1][0] - Point_List[index][0]) / 30
     divided_y = (Point_List[index + 1][1] - Point_List[index][1]) / 30
-    pass
-close_canvas()
+    move_right(divided_x, divided_y, index)
+
+
+
+index = 0
+
+while True:
+    move_point_to_point(index)
+    if index < 9:
+        index += 1
+    elif index >= 9:
+        index = 0
 
