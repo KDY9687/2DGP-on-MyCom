3e28eb70b85129e2c30ed8c3aed15a937de88bb6
KDY_MyCom <jungsang3488@gmailc.om>
2018-09-17 21:44:03 +0900

    Drill05
    사용할 이미지 로드및 포인트 좌표리스트 생성, 사용할 함수 뼈대 구현

---
+++ Drills/Drill-05/Drill05.py
@@ -0,0 +1,18 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+Point_List = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336],
+              [712, 349]]
+
+def move_right():
+    pass
+
+def move_reft():
+    pass
+
+def move_point_to_point():
+    pass
+close_canvas()
+
--- Drills/Drill-05/character_moves.py
+++ Drills/Drill-05/character_moves.py
@@ -5,6 +5,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# fill here
-    
+# 여기를 채우세요.
+
 close_canvas()
+
