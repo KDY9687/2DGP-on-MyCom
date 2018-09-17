33d458a6be77e5f1d4a1c5dfdbc78f6668e416e2
KDY_MyCom <jungsang3488@gmailc.om>
2018-09-17 16:09:04 +0900

    Drill05-2
    사각형그리기 함수 내부 구조 설계

--- Lecture03/Drill05.py
+++ Lecture03/Drill05.py
@@ -4,9 +4,30 @@ open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
+def move_from_center_to_right():
+    pass
+
+def  move_up():
+    pass
+
+def move_up():
+    pass
+
+def  move_left():
+    pass
+
+def  move_down():
+    pass
+
+def move_from_left_to_center():
+    pass
 
 def make_rectangel():
-    pass
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_from_left_to_center()
 
 def make_circle():
     pass
