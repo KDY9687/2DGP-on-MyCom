20113d8304685ddc58d65d60a172abdfc69510e6
KDY_MyCom <jungsang3488@gmailc.om>
2018-09-17 21:54:11 +0900

    Drill05
    각 함수에 필요한 변수 등록 및 초기화

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -6,13 +6,23 @@ character = load_image('animation_sheet.png')
 Point_List = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92], [692, 518], [682, 336],
               [712, 349]]
 
-def move_right():
+def move_right(divided_x, divided_y, index):
+    frame = 0
+    x = Point_List[index][0]
+    y = Point_List[index][1]
+    repeat = 0
     pass
 
-def move_reft():
+def move_reft(divdivided_x, divided_y, index):
+    frame = 0
+    x = Point_List[index][0]
+    y = Point_List[index][1]
+    repeat = 0
     pass
 
-def move_point_to_point():
+def move_point_to_point(index):
+    divided_x = (Point_List[index+1][0] - Point_List[index][0]) / 30
+    divided_y = (Point_List[index + 1][1] - Point_List[index][1]) / 30
     pass
 close_canvas()
 
