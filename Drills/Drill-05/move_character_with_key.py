aeed4e33b3aff7055c235cb0cf028ea275920fb8
KDY_MyCom <jungsang3488@gmailc.om>
2018-09-20 17:00:19 +0900

    Drill06
    초기에 Lecture 05의 move_character_with_mouse.py
    
    내용을 복사 붙여넣기

--- Drills/Drill-05/move_character_with_key.py
+++ Drills/Drill-05/move_character_with_key.py
@@ -2,8 +2,27 @@ from pico2d import *
 
 
 def handle_events():
-    # fill here
-    pass
+   global running
+   global dir
+   global idle_status
+   events = get_events()
+   for event in events:
+       if event.type == SDL_QUIT:
+           running = False
+       elif event.type == SDL_KEYDOWN:
+           if event.key == SDLK_RIGHT:
+               idle_status = 1
+               dir += 1
+           elif event.key == SDLK_LEFT:
+               idle_status = -1
+               dir -= 1
+           elif event.key == SDLK_ESCAPE:
+                running = False
+       elif event.type == SDL_KEYUP:
+           if event.key == SDLK_RIGHT:
+               dir -= 1
+           elif event.key == SDLK_LEFT:
+               dir += 1
 
 
 open_canvas()
@@ -13,16 +32,26 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
+idle_status = 0
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    if (dir == 1):
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    elif (dir == -1):
+        character.clip_draw(frame * 100, 0 * 1, 100, 100, x, 90)
+    elif (dir == 0):
+        if(idle_status == 1 or idle_status == 0):
+            character.clip_draw(frame * 100, 300 * 1, 100, 100, x, 90)
+        elif(idle_status == -1):
+            character.clip_draw(frame * 100, 200 * 1, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
-
+    x += dir * 5
     delay(0.05)
 
 close_canvas()
