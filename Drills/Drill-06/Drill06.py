fe67900037d092b0aef0176721bb62eba4ebe005
KDY_MyCom <jungsang3488@gmailc.om>
2018-09-20 17:11:45 +0900

    Drill06
    마우스 이미지 삽입 및 마우스 피봇 위치조정, 마우스 좌표값에 실제 마우스 커서값 링크

--- Drills/Drill-06/Drill06.py
+++ Drills/Drill-06/Drill06.py
@@ -21,6 +21,7 @@ def handle_events():
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
@@ -31,7 +32,8 @@ hide_cursor()
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, 100, 100)
+    hand.draw_now(x + 20, y- 25)
     update_canvas()
     frame = (frame + 1) % 8
 
