7dc11631f521233ca8244090bf53cea76543f82e
KDY_MyCom <jungsang3488@gmailc.om>
2018-10-01 15:54:47 +0900

    Drill06 완성
    마우스 클릭시 캐릭터를 움직이는 함수 구현, 인자로 마우스의 현재 위치를 가져옴, 캐릭터와 마우스의 현재위치 사이를 40으로 나누고 반복문으로 반복하며 캐릭터이미지의 피봇을 이동 완성

--- Drills/Drill-06/Drill06.py
+++ Drills/Drill-06/Drill06.py
@@ -2,11 +2,33 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
+def move_to_goal(target_x, target_y):
+    global boy_x, boy_y, face, frame
+    global move
+    move = True
+    divide_x = (target_x - boy_x) / 40
+    divide_y = (target_y - boy_y) / 40
+    for i in range(1, 40 + 1):
+        if move:
+            if divide_x < 0:
+                face =0
+            else:
+                face = 1
+            clear_canvas()
+            kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+            character.clip_draw(frame * 100, 100 * face, 100, 100, boy_x, boy_y)
+            hand.draw_now(x + 20, y - 25)
+            update_canvas()
+            boy_x += divide_x
+            boy_y += divide_y
+            frame = (frame + 1) % 8
+            handle_events()
+            delay(0.05)
+    move = False
 
 def handle_events():
     global running
     global x, y
-    global target_x, target_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -14,8 +36,7 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            if(event.button == SDL_BUTTON_LEFT):
-                target_x, target_y = event.x, KPU_HEIGHT - 1 - event.y
+            move_to_goal(x, y)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -25,6 +46,8 @@ character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
 running = True
+move = False
+face = 0
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 target_x, target_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 boy_x, boy_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
@@ -32,19 +55,15 @@ frame = 0
 hide_cursor()
 
 while running:
-    clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    if(boy_x != target_x):
-        boy_x += 1
-    if(boy_y != target_y):
-        boy_y += 1
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, boy_x, boy_y)
-    hand.draw_now(x + 20, y- 25)
-    update_canvas()
-    frame = (frame + 1) % 8
-
-    delay(0.02)
-    handle_events()
+    if move == False:
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(frame * 100, 100 * face, 100, 100, boy_x, boy_y)
+        hand.draw_now(x + 20, y- 25)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+        handle_events()
 
 close_canvas()
 
