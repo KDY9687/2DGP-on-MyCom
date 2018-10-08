c9dffb567a134a1528c5a35b7650a8e0cb3c2c15
KDY_MyCom <jungsang3488@gmailc.om>
2018-10-08 11:46:54 +0900

    Drill08_2
    2014182008 김동엽
    이동함수 구현 완성

--- Drills/Drill-08/Drill08-2.py
+++ Drills/Drill-08/Drill08-2.py
@@ -1,54 +1,8 @@
 import turtle
 import random
+from pico2d import *
 
-
-def stop():
-    turtle.bye()
-
-
-def prepare_turtle_canvas():
-    turtle.setup(1024, 768)
-    turtle.bgcolor(0.2, 0.2, 0.2)
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.shape('arrow')
-    turtle.shapesize(2)
-    turtle.pensize(5)
-    turtle.color(1, 0, 0)
-    turtle.speed(100)
-    turtle.goto(-500, 0)
-    turtle.pendown()
-    turtle.goto(480, 0)
-    turtle.stamp()
-    turtle.penup()
-    turtle.goto(0, -360)
-    turtle.pendown()
-    turtle.goto(0, 360)
-    turtle.setheading(90)
-    turtle.stamp()
-    turtle.penup()
-    turtle.home()
-
-    turtle.shape('circle')
-    turtle.pensize(1)
-    turtle.color(0, 0, 0)
-    turtle.speed(50)
-
-    turtle.onkey(stop, 'Escape')
-    turtle.listen()
-
-
-
-def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
-
-
-def draw_point(p):
-    turtle.goto(p)
-    turtle.dot(5, random.random(), random.random(), random.random())
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def draw_curved_line(p1, p2, p3, p4, start_point):
 
@@ -56,29 +10,57 @@ def draw_curved_line(p1, p2, p3, p4, start_point):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        draw_point((x, y))
-    draw_point(start_point)
 
-def draw_points(p, n):
-    for i in range(0, n, 1):
-        draw_big_point(p[i])
+def move_point_to_point(p1, p2, p3, p4, start_point):
+    global x, y, face, frame
+    global move
+    divide_x = (p1[0] - p2[0])
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        if p2[0] < p3[0]:
+            face = 1
+        else:
+            face = 0
+        character.clip_draw(frame * 100, face * 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
 
-n = 10
-p = [(random.randint(-400, 400), random.randint(-400, 400)) for i in range(n)]
+number = 10
+p = [(random.randint(0, 900), random.randint(0, 900)) for i in range(number)]
 
-prepare_turtle_canvas()
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+running = True
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+size = 20
+frame = 0
+face = 0
+points = [(random.randint(0, 1280), random.randint(0, 1024)) for i in range(size)]
+n = 0
 
-draw_points(p, n)
+while running:
+    clear_canvas()
+    move_point_to_point(p[0], p[1], p[2], p[3], p[1])
+    move_point_to_point(p[1], p[2], p[3], p[4], p[2])
+    move_point_to_point(p[2], p[3], p[4], p[5], p[3])
+    move_point_to_point(p[3], p[4], p[5], p[6], p[4])
+    move_point_to_point(p[4], p[5], p[6], p[7], p[5])
+    move_point_to_point(p[5], p[6], p[7], p[8], p[6])
+    move_point_to_point(p[6], p[7], p[8], p[9], p[7])
+    move_point_to_point(p[7], p[8], p[9], p[0], p[8])
+    move_point_to_point(p[8], p[9], p[0], p[1], p[9])
+    move_point_to_point(p[9], p[0], p[1], p[2], p[0])
+    n += 1
+    update_canvas()
+    frame = (frame + 1) % 8
+    if n == 20 - 1:
+        n = 0
+    delay(0.05)
 
-while True:
-    draw_curved_line(p[0], p[1], p[2], p[3], p[1])
-    draw_curved_line(p[1], p[2], p[3], p[4], p[2])
-    draw_curved_line(p[2], p[3], p[4], p[5], p[3])
-    draw_curved_line(p[3], p[4], p[5], p[6], p[4])
-    draw_curved_line(p[4], p[5], p[6], p[7], p[5])
-    draw_curved_line(p[5], p[6], p[7], p[8], p[6])
-    draw_curved_line(p[6], p[7], p[8], p[9], p[7])
-    draw_curved_line(p[7], p[8], p[9], p[0], p[8])
-    draw_curved_line(p[8], p[9], p[0], p[1], p[9])
-    draw_curved_line(p[9], p[0], p[1], p[2], p[0])
-turtle.done()
+
+close_canvas()
