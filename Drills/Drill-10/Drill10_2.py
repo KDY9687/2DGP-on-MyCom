4a2b5a6c78089397879412549327ddfa54ad031c
KDY_MyCom <jungsang3488@gmailc.om>
2018-10-11 21:47:15 +0900

    Drill10
    draw함수에 main_state.boy, main_state.grass 그리기 등록 Drill10_2
     pause 이미지 깜박임 효과 추가
    완성
    
    2014182008 김동엽
    Drill10_1, 2 완성 총 커밋횟수 : 6
    
    1 동영상 링크 :https://youtu.be/ZYZsZSbZt44
    
    2 동영상링크
    :https://youtu.be/H22cbawEowY

--- Drills/Drill-10/Drill10_2.py
+++ Drills/Drill-10/Drill10_2.py
@@ -5,7 +5,7 @@ import main_state
 
 name = "Drill10_2"
 image = None
-
+count = 0
 
 def enter():
     global image
@@ -18,6 +18,7 @@ def exit():
 
 
 def handle_events():
+    global count
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -26,11 +27,20 @@ def handle_events():
             if(event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+                count = 0
                 game_framework.pop_state()
 
 def draw():
+    global count
     clear_canvas()
-    image.draw(400, 300)
+    main_state.boy.draw()
+    main_state.grass.draw()
+    if count == 0:
+        image.draw(400, 300)
+        count += 1
+    elif count == 1:
+        count -= 1
+    delay(0.3)
     update_canvas()
 
 
