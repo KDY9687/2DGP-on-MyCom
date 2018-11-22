0f8a6a3c50a375cb15fabeed7a84b529c8e4701f
KDY_MyCom <jungsang3488@gmailc.om>
2018-11-22 16:22:56 +0900

    Drill-14
    베이스 수정

--- Drills/Drill-14/main_state.py
+++ Drills/Drill-14/main_state.py
@@ -7,13 +7,29 @@ import game_framework
 import game_world
 
 from boy import Boy
-from background import FixedBackground as Background
-
+from grass import Grass
+from ball import Ball
 
 name = "MainState"
 
 boy = None
-background = None
+grass = None
+balls = []
+
+
+def collide(a, b):
+    # fill here
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+
 
 
 def enter():
@@ -21,12 +37,14 @@ def enter():
     boy = Boy()
     game_world.add_object(boy, 1)
 
-    global background
-    background = Background()
-    game_world.add_object(background, 0)
+    global grass
+    grass = Grass()
+    game_world.add_object(grass, 0)
 
-    background.set_center_object(boy)
-    boy.set_background(background)
+    global balls
+    balls = [Ball() for i in range(10)]
+    game_world.add_objects(balls, 1)
+
 
 
 
@@ -55,7 +73,11 @@ def handle_events():
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
-
+    for ball in balls:
+        if collide(boy, ball):
+            balls.remove(ball)
+            boy.eat(ball)
+            game_world.remove_object(ball)
 
 def draw():
     clear_canvas()
